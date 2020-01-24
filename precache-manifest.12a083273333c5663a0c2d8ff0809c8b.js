d5d8c86b318d998ee2223df9dae0177584ace15c
Jesseliz <choque101108@gmail.com>
2020-01-24 15:33:59 -0500

    Initial dist subtree commit

---
+++ precache-manifest.12a083273333c5663a0c2d8ff0809c8b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b680888320819eee0c",
+    "url": "/Konecta-validate/css/about.e31c14e2.css"
+  },
+  {
+    "revision": "8785bd2683f3a61482c2",
+    "url": "/Konecta-validate/css/chunk-19350dfb.c2c905a6.css"
+  },
+  {
+    "revision": "73916ba36cfecb6ddfcd",
+    "url": "/Konecta-validate/css/chunk-85c0a088.26a6ff06.css"
+  },
+  {
+    "revision": "cc1d946b282e8ba8c4b9",
+    "url": "/Konecta-validate/css/chunk-vendors.2f2f8185.css"
+  },
+  {
+    "revision": "d07753d3b1a5576de43b",
+    "url": "/Konecta-validate/css/home.cac309b4.css"
+  },
+  {
+    "revision": "0c5335f7777c32ea692789981eaed8b1",
+    "url": "/Konecta-validate/index.html"
+  },
+  {
+    "revision": "55b680888320819eee0c",
+    "url": "/Konecta-validate/js/about.2fd0af39.js"
+  },
+  {
+    "revision": "beb09f759c2529861921",
+    "url": "/Konecta-validate/js/app.e2d9bc73.js"
+  },
+  {
+    "revision": "8785bd2683f3a61482c2",
+    "url": "/Konecta-validate/js/chunk-19350dfb.e02ccc7d.js"
+  },
+  {
+    "revision": "73916ba36cfecb6ddfcd",
+    "url": "/Konecta-validate/js/chunk-85c0a088.67218852.js"
+  },
+  {
+    "revision": "cc1d946b282e8ba8c4b9",
+    "url": "/Konecta-validate/js/chunk-vendors.796114eb.js"
+  },
+  {
+    "revision": "d07753d3b1a5576de43b",
+    "url": "/Konecta-validate/js/home.0ef7b95a.js"
+  },
+  {
+    "revision": "8b755839a0d6ae8862e2505dfefda651",
+    "url": "/Konecta-validate/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Konecta-validate/robots.txt"
+  }
+]);
