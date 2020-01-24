d0c5ea8fb53996c38c56347b3b3254f6e12ac8b2
Jesseliz <choque101108@gmail.com>
2020-01-24 15:23:27 -0500

    add commit my first commit

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ],
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/Konecta-colaborador/'
-: '/'
+  ]
 }
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//       ? '/Konecta-colaborador/'
-//     : '/'
-// } 
