09c34ebd8fc412bb94b066425b6b852135aed0c9
Jesseliz <choque101108@gmail.com>
2020-02-01 10:55:30 -0500

    Initial dist subtree commit

---
+++ dist/precache-manifest.70c3794d05bab98810cfec218b4b4af6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdbbf53f3a465f04e61",
+    "url": "/Konecta-validate/css/about.e31c14e2.css"
+  },
+  {
+    "revision": "8785bd2683f3a61482c2",
+    "url": "/Konecta-validate/css/chunk-19350dfb.c2c905a6.css"
+  },
+  {
+    "revision": "a8c8059a7b0297098d14",
+    "url": "/Konecta-validate/css/chunk-a1769e84.184f6d76.css"
+  },
+  {
+    "revision": "6c5540dca327d7f1efe8",
+    "url": "/Konecta-validate/css/chunk-vendors.dfaea188.css"
+  },
+  {
+    "revision": "980c7aa10df09075cb16",
+    "url": "/Konecta-validate/css/home.cac309b4.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/Konecta-validate/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/Konecta-validate/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/Konecta-validate/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/Konecta-validate/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "b5bdabfbac02f3a26859d23931292a11",
+    "url": "/Konecta-validate/index.html"
+  },
+  {
+    "revision": "cbdbbf53f3a465f04e61",
+    "url": "/Konecta-validate/js/about.bf910667.js"
+  },
+  {
+    "revision": "d95303b0ab962efee1ad",
+    "url": "/Konecta-validate/js/app.a6b50e20.js"
+  },
+  {
+    "revision": "8785bd2683f3a61482c2",
+    "url": "/Konecta-validate/js/chunk-19350dfb.e02ccc7d.js"
+  },
+  {
+    "revision": "a8c8059a7b0297098d14",
+    "url": "/Konecta-validate/js/chunk-a1769e84.046a1134.js"
+  },
+  {
+    "revision": "6c5540dca327d7f1efe8",
+    "url": "/Konecta-validate/js/chunk-vendors.efa1fce7.js"
+  },
+  {
+    "revision": "980c7aa10df09075cb16",
+    "url": "/Konecta-validate/js/home.cdbab530.js"
+  },
+  {
+    "revision": "bba683fecf01afc2971cc5302b544bbe",
+    "url": "/Konecta-validate/logo.png"
+  },
+  {
+    "revision": "8b755839a0d6ae8862e2505dfefda651",
+    "url": "/Konecta-validate/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Konecta-validate/robots.txt"
+  }
+]);
